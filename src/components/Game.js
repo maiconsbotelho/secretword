eefafa20eca666ff911b1d533fd06607d46ba0aa
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-06-28 17:25:02 -0300

    find: add html game components structure

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,33 @@
-import "./Game.css";
+import './Game.css';
 
-const Game = ({verifyLetter}) => {
+const Game = ({ verifyLetter }) => {
   return (
     <div className="Game">
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+      <p className="points">
+        <span>Pontuação: 0</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+        <div className="letterContainer">
+          <p>Tente adivinhar uma letra da palavra:</p>
+          <form>
+            <input type="text" name="letter" maxLength="1" required />
+            <button>Jogar!</button>
+          </form>
+        </div>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   );
-}
+};
 
 export default Game;
