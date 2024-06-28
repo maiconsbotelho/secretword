d37001ddfcdb911e41014cf280d7fcaa528e762d
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-06-28 16:29:22 -0300

    find: add function pickWord

--- src/App.js
+++ src/App.js
@@ -22,8 +22,44 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  
+  // Zerando estados
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [pickedLetter, setPickedLetter] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // Pegar uma categoria aleatória
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * categories.length)];
+    console.log(category);
+
+    // Pegar uma palavra aleatória
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return { word, category };
+  }
+
+
   // Start game
   const startGame = () => {
+    // Pegar a palavra e a categoria
+    const { word, category } = pickWordAndCategory();
+
+    // Separar as letras da palavra
+    let wordLetters = word.split('');
+    // Transformar todas as letras em minúsculas
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase()); 
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // Atualizar os estados
+    setPickedWord(word);
+    setPickedCategory(category);
+    setPickedLetter(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
