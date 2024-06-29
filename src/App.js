d3f0ae308133dc6182adf2a1f658a40652fece86
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-06-28 22:24:48 -0300

    find: finish

--- src/App.js
+++ src/App.js
@@ -18,15 +18,22 @@ const stages = [
   { id: 3, name: 'End' },
 ];
 
+const guessesQty = 6;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  
   // Zerando estados
-  const [pickedWord, setPickedWord] = useState('');
-  const [pickedCategory, setPickedCategory] = useState('');
-  const [pickedLetter, setPickedLetter] = useState([]);
+  const [pickedWord, setPickedWord] = useState(''); // Palavra escolhida
+  const [pickedCategory, setPickedCategory] = useState(''); // Categoria da palavra
+  const [letters, setLetters] = useState([]); // Letras da palavra
+
+  // Estados para as letras
+  const [guessedLetters, setGuessedLetters] = useState([]); // Letras corretas
+  const [wrongLetters, setWrongLetters] = useState([]); // Letras erradas
+  const [guesses, setGuesses] = useState(guessesQty); // Número de tentativas
+  const [score, setScore] = useState(0); // Pontuação
 
   const pickWordAndCategory = () => {
     // Pegar uma categoria aleatória
@@ -39,18 +46,19 @@ function App() {
     console.log(word);
 
     return { word, category };
-  }
-
+  };
 
   // Start game
   const startGame = () => {
+    // Zerar os estados
+    clearLettersStates();
     // Pegar a palavra e a categoria
     const { word, category } = pickWordAndCategory();
 
     // Separar as letras da palavra
     let wordLetters = word.split('');
     // Transformar todas as letras em minúsculas
-    wordLetters = wordLetters.map((letter) => letter.toLowerCase()); 
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
     console.log(word, category);
     console.log(wordLetters);
@@ -58,29 +66,75 @@ function App() {
     // Atualizar os estados
     setPickedWord(word);
     setPickedCategory(category);
-    setPickedLetter(wordLetters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   };
 
   // Game
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
-  }
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+    // Verificar se a letra já foi escolhida
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+
+    // Verificar se a letra está na palavra
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [...actualGuessedLetters, normalizedLetter]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [...actualWrongLetters, normalizedLetter]);
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+
+  const clearLettersStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  // Verificar se o jogo acabou
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLettersStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
+  // verificar condições de vitoria
+  useEffect(() => {
+    const uniqueLetters = [...new Set(letters)];
+
+    if (uniqueLetters.length === guessedLetters.length) {
+      // Aumentar a pontuação
+      setScore((actualScore) => (actualScore += 100));
+      startGame(); // Iniciar um novo jogo automaticamente após acertar a palavra anterior
+    }
+  }, [guessedLetters, letters, startGame]);
 
   // Resetar
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
     setGameStage(stages[0].name);
-  }
-
-
-
+  };
 
   return (
     <div className="App">
       {gameStage === 'Start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'Game' && <Game verifyLetter={verifyLetter} />}
-      {gameStage === 'End' && <GameOver retry={retry} />}
+      {gameStage === 'Game' && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === 'End' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,12 +1,15 @@
 import './GameOver.css';
 
-const GameOver = ({retry}) => {
+const GameOver = ({ retry, score }) => {
   return (
     <div className="GameOver">
-      <h1>GameOver</h1>
+      <h1>Fim de Jogo!</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
       <button onClick={retry}>Resetar Jogo</button>
     </div>
   );
-}
+};
 
 export default GameOver;
